b94c7cd20f2db0bf8b2f59fd4d207025fd896ca1
Savion Levi <savion.levi@mail.huji.ac.il>
2019-02-04 10:22:28 +0200

    creating , union, intersect, diff & printing sets
    exercise guidelines:
    Define a function singletonSet which creates a singleton set from one integer value: the set represents the set of the one given element. Now that we have a way to create singleton sets, we want to define a function that allow us to build bigger sets from smaller ones.
    Define the functions union,intersect, and diff, which takes two sets, and return, respectively, their union, intersection and differences. diff(s, t) returns a set which contains all the elements of the set s that are not in the set t.
    Define the function filter which selects only the elements of a set that are accepted by a given predicate p. The filtered elements are returned as a new set.

---
+++ singletonSet.scala
@@ -0,0 +1,56 @@
+import  scala.collection.mutable.Set
+
+object singletonSet extends App {
+
+  var firstSingleton = new mySingletonSet()
+  firstSingleton.createSingletonSet(5)
+
+  var singleton1 = Set(1)
+  var singleton2 = Set(2)
+  var union = firstSingleton.union(singleton1 , singleton2)
+  println("first element: "+union.head)
+  println("second element: " +union.tail)
+
+  var singleton3 = Set(2,3)
+  var singleton4 = Set(3,4)
+  var intersectSet = firstSingleton.intersect(singleton3, singleton4)
+  println("intersect set : "+intersectSet.head)
+  println("intersect set : "+intersectSet.tail)
+
+  firstSingleton.printSet(firstSingleton.diff(singleton3, singleton4))
+}
+
+class mySingletonSet {
+
+  def createSingletonSet(number : Int) : Set[Int] = {
+    val singletonSet : Set[Int]  = Set(number)
+    println(singletonSet.head)
+
+    return singletonSet
+  }
+
+  def union(firstSingleton : Set[Int] , secondSingleton : Set[Int]) : Set[Int] = {
+    val unionSet = Set(firstSingleton.head , secondSingleton.head)
+
+    return unionSet
+  }
+
+  def intersect(firstSingleton : Set[Int] , secondSingleton : Set[Int]) : Set[Int] = {
+
+    val intersectSet = firstSingleton.intersect(secondSingleton)
+    return intersectSet
+  }
+
+  def diff(firstSingleton : Set[Int] , secondSingleton : Set[Int]) : Set[Int] = {
+    println("printing you set kind sir ! ")
+    var diffSet = firstSingleton ++ secondSingleton
+    return diffSet
+  }
+
+  def printSet(mySet : Set[Int]) = {
+    var myInt = 0;
+    for(myInt <- mySet) {
+      println(myInt)
+    }
+  }
+}
